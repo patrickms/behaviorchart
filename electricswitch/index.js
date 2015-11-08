0d7471d5ff207c4b2a2cb6ef7efba3847e51ad48
Patrick Morris-Suzuki <github@morris-suzuki.com>
2015-11-08 03:43:54 -0500

    Now updates based on the timetable

--- electricswitch/index.js
+++ electricswitch/index.js
@@ -3,17 +3,62 @@ var root=new Firebase('https://behaviorchart.firebaseIO.com/')
 
 var five = require("johnny-five");
 var os=require('os');
+var datejs=require('datejs');
 var board;
 if(os.type().toLowerCase()=='windows_nt')
 	board = new five.Board( {port: "COM3"});
 else
 	board = new five.Board();
 
+var lastRows=[];
+var powerPin=null;
+
+function updatePower(rows)
+{
+    var newState=false;
+    if(rows)
+    {
+    	var now=new Date();
+    	var today=Date.today();
+    	var nearestBefore =today.at('00:00AM');
+    	var allowBefore = false;
+    	var nearestAfter = today.at('11:59:59PM');
+    	rows.forEach(function(row, rowIndex)
+    	{
+    		console.log(row);
+    		startTime=today.at(row.startTime);
+    		if(startTime<=now && startTime>=nearestBefore)
+    		{
+    			nearestBefore=startTime;
+    			allowBefore=row.electronicsAllowed&&row.preconditionsMet;
+    		}
+    		if(startTime>now && startTime<=nearestAfter)
+    		{
+    			nearestAfter=startTime;
+    		}
+    	});
+    	console.log(nearestBefore, nearestAfter, allowBefore);
+    	if(powerPin)
+    	{
+			if(allowBefore)
+				powerPin.on();
+			else
+				powerPin.off();
+		};
+    	setTimeout( updatePowerFromLastRows, nearestAfter - ( new Date() ) );
+	}
+}
+
+function updatePowerFromLastRows()
+{
+	updatePower(lastRows);
+}
+
 board.on("ready", function() {
   console.log('Arduino ready');
   
   // Create a standard `led` component instance
-  var powerPin = new five.Led(12);
+  powerPin = new five.Led(12);
   var powerRef = root.child('power');
   var timetableRef = root.child('timetable');
   /*
@@ -27,6 +72,12 @@ board.on("ready", function() {
     	powerPin.off();
   });*/
 
+  timetableRef.on('value', function(dataSnapshot) {
+    // code to handle new value.
+    var rows=dataSnapshot.val();
+    lastRows=rows;
+    updatePower(rows);
+  });
   // "blink" the led in 500ms
   // on-off phase periods
   
