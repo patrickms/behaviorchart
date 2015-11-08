686cc7774c59012178d25a1d367aadf1623393b3
Patrick Morris-Suzuki <github@morris-suzuki.com>
2015-11-07 23:01:37 -0500

    Basic firmata demo now works
    Lessons: Use latest Firmata (at least v 2.4.4. V 2.3.8 doesn’t work,
    johnny-five disconnects).
    
    Always use an external power supply for the arduino!

---
+++ electricswitch/index.js
@@ -0,0 +1,26 @@
+var Firebase=require('firebase')
+var root=new Firebase('https://behaviorchart.firebaseIO.com/')
+
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  console.log('Arduino ready');
+  
+  // Create a standard `led` component instance
+  var powerPin = new five.Led(12);
+  var powerRef = root.child('power');
+  powerRef.on('value', function(dataSnapshot) {
+    // code to handle new value.
+    val=dataSnapshot.val().toLowerCase().trim();
+    console.log('powerRef value',val);
+    if(val=='on')
+    	powerPin.on();
+    else if(val=='off')
+    	powerPin.off();
+  });
+
+  // "blink" the led in 500ms
+  // on-off phase periods
+  
+});
